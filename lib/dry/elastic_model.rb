0b6fcdd492b4fd296850631bb2c1317591015951
Konrad Oleksiuk <konole@gmail.com>
2019-03-03 20:58:25 +0100

    Initial commit

---
+++ lib/dry/elastic_model.rb
@@ -0,0 +1,8 @@
+require "dry/elastic_model/version"
+
+module Dry
+  module ElasticModel
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/dry/elastic_model/version.rb
@@ -0,0 +1,5 @@
+module Dry
+  module ElasticModel
+    VERSION = "0.1.0"
+  end
+end
