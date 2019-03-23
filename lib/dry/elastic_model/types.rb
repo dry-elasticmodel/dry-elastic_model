4564ddeede75fd634ee6c313d1df2c68f7c7b937
Konrad Oleksiuk <konole@gmail.com>
2019-03-23 20:13:01 +0100

    Add comments for basic types

--- lib/dry/elastic_model/types.rb
+++ lib/dry/elastic_model/types.rb
@@ -3,13 +3,18 @@ module Dry
     module Types
       include Dry::Types.module
 
+      # String datatypes
       Text = Types::String.meta(es_name: "text", opts: { index: "not_analyzed" })
       Keyword = Types::Symbol.meta(es_name: "keyword")
 
+      # Binary datatype
       # TODO: Verify if correct Base64
       Binary = Types::String.meta(es_name: "binary")
 
+      # Date datatype
       Date = Types::Date.meta(es_name: "date", opts: { format: "strict_date_optional_time||epoch_millis" })
+
+      # Numeric datatypes
       Long = Types::Integer.constrained(gteq: -2**63, lteq: 2**63-1).meta(es_name: "long")
       Integer = Types::Integer.constrained(gteq: -2**31, lteq: 2**31-1).meta(es_name: "integer")
       Short = Types::Integer.meta(es_name: "short").
@@ -20,7 +25,11 @@ module Dry
       Float = Types::Float.meta(es_name: "float")
       HalfFloat = Types::Float.meta(es_name: "half_flot")
       ScaledFloat = Types::Float.meta(es_name: "scaled_flot")
+
+      # Boolean datatype
       Boolean = (Types::Strict::Bool | Types.Value('true') | Types.Value('false')).meta(es_name: "boolean")
+
+      # IP datatype
       IP = Types::String.
              constrained(format: /\A(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\z/i).
              meta(es_name: "ip")
