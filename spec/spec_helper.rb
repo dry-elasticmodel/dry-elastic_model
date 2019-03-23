b5ae3dceaa56b0803fa5f57ccbec97e8a4a6af53
Konrad Oleksiuk <konole@gmail.com>
2019-03-23 20:41:35 +0100

    Add test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require "simplecov"
+require "simplecov-lcov"
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start do
+  add_filter(/^\/spec\//)
+end
+
 require "bundler/setup"
 require "dry/elastic_model"
 
