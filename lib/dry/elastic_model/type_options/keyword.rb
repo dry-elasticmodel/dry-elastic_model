d7d356287de5fd5ed6458294e89801ca3ae909f7
Konrad Oleksiuk <konole@gmail.com>
2019-04-17 12:13:08 +0200

    Remove duplicated options for keyword

--- lib/dry/elastic_model/type_options/keyword.rb
+++ lib/dry/elastic_model/type_options/keyword.rb
@@ -6,11 +6,6 @@ module Dry
         attribute :ignore_above, Types::Bool.meta(omittable: true)
         attribute :null_value, Types::Bool.meta(omittable: true)
         attribute :normalizer, Types::String.meta(omittable: true)
-        attribute :fielddata_frequency_filter, Types::String.meta(omittable: true)
-        attribute :position_increment_gap, Types::Integer.meta(omittable: true)
-        attribute :search_analyzer, Types::String.meta(omittable: true)
-        attribute :search_quote_analyzer, Types::String.meta(omittable: true)
-        attribute :term_vector, Types::Bool.meta(omittable: true)
 
         def to_h
           attributes
